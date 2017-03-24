de53eaaec07b7b938cb034ef7356a29132fb3950
Sergio del Rio <sdelrio0@gmail.com>
2017-03-24 11:11:21 -0600

    Remove style loaders

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import styles from './app.css';
 
 const App = () => (
-  <div className={styles.app}>
-    <h2>Hello, a?</h2>
+  <div>
+    <h1>Hello, a?</h1>
   </div>
 );
 
